52b10817c3a758003fe021b7995df7fbb87a66e6
Chris Sjoblom <chris.sjoblom@gmail.com>
2015-12-06 19:59:57 -0800

    beauty

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ const PLUGIN_NAME = 'gulp-wp-to-md';
 // file can be a vinyl file object or a string
 // when a string it will construct a new one
 module.exports = function(file, opt) {
-    if(!file) {
-        throw new PluginError('gulp-wp-to-md', 'Missing file option for gulp-wp-to-md');
+    if (!file) {
+        throw new PluginError(PLUGIN_NAME, 'Missing file option for gulp-wp-to-md');
     }
     opt = opt || {};
 
     //to preserve existing |undefined| behavior and to introduce |newLine: ""| for binaries
     if (typeof opt.newLine !== 'string') {
-         opt.newLine = gutil.linefeed;
+        opt.newLine = gutil.linefeed;
     }
 
     var convert;
@@ -41,21 +41,21 @@ module.exports = function(file, opt) {
     } else if (typeof file.path === 'string') {
         fileName = path.basename(file.path);
     } else {
-        throw new PluginError('gulp-wp-to-md', 'Missing path in file options for gulp-wp-to-md');
+        throw new PluginError(PLUGIN_NAME, 'Missing path in file options for gulp-wp-to-md');
     }
 
     function bufferContents(file, encode, callback) {
         //ignore empty
-        if(file.isNull()) {
+        if (file.isNull()) {
             callback();
             return;
         }
 
         // we dont do streams yet
         if (file.isStream()) {
-             this.emit('error', new PluginError('gulp-wp-to-md', 'Steaming not supported'));
-             callback();
-             return;
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Steaming not supported'));
+            callback();
+            return;
         }
 
         // set latest file if not set already
@@ -66,7 +66,7 @@ module.exports = function(file, opt) {
         }
 
         // construct a conversion instance
-        if(!convert) {
+        if (!convert) {
             convert = new Convert(fileName);
         }
 
@@ -77,7 +77,7 @@ module.exports = function(file, opt) {
 
     function endStream(callback) {
         //no file passed in, no file goes out
-        if(!latestFile || !convert) {
+        if (!latestFile || !convert) {
             callback();
             return;
         }
@@ -87,7 +87,9 @@ module.exports = function(file, opt) {
         // if file opt was a file path
         // clone everything from the latest file
         if (typeof file === 'string') {
-            convertedFile = latestFile.clone({contents: false});
+            convertedFile = latestFile.clone({
+                contents: false
+            });
             convertedFile.path = path.join(latestFile.base, file);
         } else {
             convertedFile = new File(file);
